b6e2ac124de8b2f182fef3004705220b64b90e9b
Aisuko <urakiny@gmail.com>
2020-04-26 19:04:53 +0800

    Add the mock package
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/gofrs/flock v0.7.1
+	github.com/golang/mock v1.2.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.5.0
--- pkg/consul/interfaces.go
+++ pkg/consul/interfaces.go
@@ -2,6 +2,8 @@ package consul
 
 import "helm.sh/helm/v3/pkg/release"
 
+//go:generate mockgen -source ./interfaces.go -destination ./mocks/mock_interfaces.go
+
 // Consul interface is used to define the way how to transform the chart
 type Consul interface {
 	AddRepo() (err error)
